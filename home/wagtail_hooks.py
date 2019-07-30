618e33f2a0c08fb29bb8f8781af9a7df81009ca0
Ramirez Joe Ramir <ramir.uplb@gmail.com>
2019-07-30 13:34:28 +0800

    style for wagtail admin

--- home/wagtail_hooks.py
+++ home/wagtail_hooks.py
@@ -2,7 +2,18 @@ from wagtail.contrib.modeladmin.options import (
     ModelAdmin, modeladmin_register)
 from product.models import Product
 from product.models import Order
+from wagtail.core import hooks
+from django.utils.html import format_html
+from django.templatetags.static import static
 
+
+@hooks.register('insert_global_admin_css')
+def global_admin_css():
+    return format_html('<link rel="stylesheet" href="{}">', static('css/seller_center.css'))
+
+@hooks.register('construct_main_menu')
+def hide_snippets_menu_item(request, menu_items):
+  menu_items[:] = [item for item in menu_items if item.name != 'snippets']
 
 class ProductAdmin(ModelAdmin):
     model = Product
