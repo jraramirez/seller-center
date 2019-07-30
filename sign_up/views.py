2aede2efbb020dffc0c9a6fa07e287cd1c0bd922
John Ernest Ramos <john.ramos@erni.ph>
2019-07-30 18:37:54 +0800

    Add messaging and calling of endpoint

--- sign_up/views.py
+++ sign_up/views.py
@@ -7,6 +7,8 @@ from django.http import HttpResponseRedirect
 from users.models import Profile
 from sign_up.models import SignUpPage
 
+from django.contrib import messages
+import requests
 
 def sign_up(request):
   if(request.method == 'POST'):
@@ -23,24 +25,45 @@ def sign_up(request):
         username=request.POST['phone-number'],
         password=request.POST['password'],
       )
+      #call api here
       login(request, new_user)
       HttpResponseRedirect('/')
     else:
-      user = User.objects.create_user(
-        username=request.POST['email-address'],
-        email=request.POST['email-address'],
-        password=request.POST['password']
-      )
-      group = Group.objects.get(name='Seller')
-      user.groups.add(group)
-      new_user = authenticate(
-        username=request.POST['email-address'],
-        password=request.POST['password'],
-      )
-      login(request, new_user)
-      return render(request, 'sign_up/sign_up_page_landing.html', {
-        'request': request,
-      })
+      #call api here if success create a user else show necessary errors
+      data = {
+        'type':'email',
+        'value': 'jeramos.dev@gmail.com'
+      }
+
+      r = requests.post('https://vak4dovce5.execute-api.ap-southeast-1.amazonaws.com/dev/api/v1/register', json=data)
+
+      response = r.json()
+      print(response)
+
+      if (r.status_code == 200):
+        user = User.objects.create_user(
+          username=request.POST['email-address'],
+          email=request.POST['email-address'],
+          password=request.POST['password']
+        )
+        group = Group.objects.get(name='Seller')
+        user.groups.add(group)
+        new_user = authenticate(
+          username=request.POST['email-address'],
+          password=request.POST['password'],
+        )
+        
+        #login success
+        login(request, new_user)
+        return render(request, 'sign_up/sign_up_page_landing.html', {
+          'request': request,
+        })
+      else:
+        messages.error(request, response['details'])
+        self = SignUpPage.objects.get(slug='sign')
+        return render(request, 'sign_up/sign_up_page.html', {
+          'self': self,
+        })
   else:
     self = SignUpPage.objects.get(slug='sign')
     if(not request.user.is_authenticated):
