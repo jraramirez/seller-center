95c135634e75b58b31d5c16a8e407cf5ff4894e2
Ramirez Joe Ramir <ramir.uplb@gmail.com>
2019-07-31 14:56:51 +0800

    signup send email verification input; styles

--- seller_center/settings/dev.py
+++ seller_center/settings/dev.py
@@ -15,7 +15,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": "seller_center",
         "USER": "postgres",
-        "PASSWORD": "admin",
+        "PASSWORD": "postgres",
         "HOST": "localhost",
         "PORT": "",
     }
