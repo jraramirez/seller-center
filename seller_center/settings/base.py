415ab5c130df8ab062678d0fc713258f66236e28
John Ernest Ramos <john.ramos@erni.ph>
2019-08-02 22:53:39 +0800

    change static container

--- seller_center/settings/base.py
+++ seller_center/settings/base.py
@@ -134,6 +134,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATICFILES_STORAGE = 'http://seller-center-dev.vpsbnhp5gf.ap-southeast-1.elasticbeanstalk.com'
+
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
