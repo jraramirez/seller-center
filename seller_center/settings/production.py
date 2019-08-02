1147bf48bdfc11a93fbe71f768fec91cd4ef88bf
John Ernest Ramos <john.ramos@erni.ph>
2019-08-02 22:19:50 +0800

    add wagtailadmin/static

--- seller_center/settings/production.py
+++ seller_center/settings/production.py
@@ -21,6 +21,12 @@ if 'RDS_HOSTNAME' in os.environ:
 
 
 # Static files (Local)
+STATICFILES_LOCATION = 'static'
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_DIR, 'wagtailadmin/static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
 
 # Media files (Local)
