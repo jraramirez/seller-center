7bcc047f42485a705178d0ebabb94ff0ec751648
John Ernest Ramos <john.ramos@erni.ph>
2019-08-05 22:17:34 +0800

    commented out category insertion

--- home/models.py
+++ home/models.py
@@ -10,15 +10,15 @@ class HomePage(BasePage):
     body = StreamField(GeneralStreamBlock, blank=True)
 
     def get_context(self, request):
-        with open('seller_center/static/documents/categories.json', 'r') as f:
-          categoriesJSON = json.load(f)
-          for i in categoriesJSON['data']['list']:
-            c = Category(
-              unique_id = i['id'] + 1891,
-              parent_id = i['parent_id'] + 1891,
-              name = i['name'],
-            )
-            c.save()
+        # with open('seller_center/static/documents/categories.json', 'r') as f:
+        #   categoriesJSON = json.load(f)
+        #   for i in categoriesJSON['data']['list']:
+        #     c = Category(
+        #       unique_id = i['id'] + 1891,
+        #       parent_id = i['parent_id'] + 1891,
+        #       name = i['name'],
+        #     )
+        #     c.save()
         context = super().get_context(request)
         subPages = self.get_children().live()
         context['subPages'] = subPages
