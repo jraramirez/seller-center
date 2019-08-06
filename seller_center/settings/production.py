01853fef367c3765685bfe6dba74dc8ee0295dd1
Ramirez Joe Ramir <ramir.uplb@gmail.com>
2019-08-06 11:57:41 +0800

    AWS S3 static files storage

--- seller_center/settings/production.py
+++ seller_center/settings/production.py
@@ -11,14 +11,31 @@ DATABASES = {}
 DATABASES['default'] = dj_database_url.parse(DATABASE_URL, conn_max_age=600)
 
 
+# AWS S3 Storage
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_FILE_OVERWRITE = True
+
+
 # Static files (Local)
-STATICFILES_LOCATION = 'static'
+# STATICFILES_LOCATION = 'static'
+# STATICFILES_DIRS = [
+#     os.path.join(PROJECT_DIR, 'static'),
+# ]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_URL = '/static/'
+
+
+# Static files (AWS)
+STATICFILES_LOCATION = os.environ.get('STATICFILES_LOCATION')
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 STATICFILES_DIRS = [
     os.path.join(PROJECT_DIR, 'static'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
-
+STATIC_ROOT = '/static/'
+STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 
 # Media files (Local)
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -26,11 +43,6 @@ STATIC_URL = '/static/'
 
 
 # Media files (AWS)
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_S3_FILE_OVERWRITE = True
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 MEDIAFILES_LOCATION = 'media'
