aff0facbc11c7eac6674861fef3330a8ec68d7c1
jeramos <jeramos.dev@gmail.com>
2019-08-09 17:36:38 +0800

    Fixed static url AWS_S3_CUSTOM_DOMAIN

--- seller_center/settings/production.py
+++ seller_center/settings/production.py
@@ -24,9 +24,12 @@ if 'RDS_HOSTNAME' in os.environ:
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_CUSTOM_DOMAIN = 'https://d3rdlm5j4gpnnq.cloudfront.net'
 AWS_S3_FILE_OVERWRITE = True
 
+
+STATIC_ROOT = '/static/'
+STATIC_URL = "https://d3rdlm5j4gpnnq.cloudfront.net/"
 
 # Static files (AWS)
 STATICFILES_LOCATION = os.environ.get('STATICFILES_LOCATION')
@@ -34,9 +37,6 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_DIRS = [
     os.path.join(PROJECT_DIR, 'static'),
 ]
-
-STATIC_ROOT = '/static/'
-STATIC_URL = "https://d3rdlm5j4gpnnq.cloudfront.net/"
 
 # Media files (Local)
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
