75326babd5985a1d57f16194db5599a8cc46bf3f
unknown <mirr@digiters.co>
2019-08-16 15:39:24 +0800

    upload, download 3 user documents

--- users/views.py
+++ users/views.py
@@ -3,38 +3,71 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from users.models import Profile
 
+from wagtail.documents.models import get_document_model
+from wagtail.documents.forms import get_document_form
+
 def profile(request):
   userData = User.objects.filter(id=request.user.id)[0]
   profileData = Profile.objects.filter(id=request.user.id)[0]
+  Document = get_document_model()
+  dtiDocument = Document.objects.filter(id=profileData.dti_id)[0] if len(Document.objects.filter(id=profileData.dti_id)) else None
+  secDocument = Document.objects.filter(id=profileData.sec_id)[0] if len(Document.objects.filter(id=profileData.sec_id)) else None
+  permitDocument = Document.objects.filter(id=profileData.permit_id)[0] if len(Document.objects.filter(id=profileData.permit_id)) else None
   return render(request, 'users/profile_page.html', {
     'profileData': profileData,
     'userData': userData,
+    'dtiDocument': dtiDocument,
+    'secDocument': secDocument,
+    'permitDocument': permitDocument
   })
 
 def profile_edit(request):
   if(request.method == "POST"):
+    Document = get_document_model()
+    profileData = Profile.objects.filter(id=request.user.id)[0]
+    p = Profile()
+    p.user_id = request.user.id
+    p.dti_id = profileData.dti_id
+    p.sec_id = profileData.sec_id
+    p.permit_id = profileData.permit_id
+
+    if('dti' in request.FILES.keys()):
+      dtiDocument = Document.objects.create(
+        file=request.FILES['dti'],
+        title=request.FILES['dti'].name
+      )
+      p.dti_id = dtiDocument.id
+
+    if('sec' in request.FILES.keys()):
+      secDocument = Document.objects.create(
+        file=request.FILES['sec'],
+        title=request.FILES['sec'].name
+      )
+      p.sec_id = secDocument.id
+
+    if('permit' in request.FILES.keys()):
+      permitDocument = Document.objects.create(
+        file=request.FILES['permit'],
+        title=request.FILES['permit'].name
+      )
+      p.permit_id = permitDocument.id
+    p.save()
     User.objects.filter(id=request.user.id).update(
       first_name = request.POST.get('first-name'),
-      last_name = request.POST.get('last-name'),
+      last_name = request.POST.get('last-name')
     )
-    dtiDocument = None
-    # secDocument = None
-    # permitDocument = None
-    # if(request.FILES['dti']):
-    #   dtiDocument = request.FILES['dti']
-    # if(request.FILES['sec']):
-    #   secDocument = request.FILES['sec']
-    # if(request.FILES['permit']):
-    #   permitDocument = request.FILES['permit']
-    # p = Profile()
-    # p.user_id = request.user.id
-    # p.dti = dtiDocument.file
-    # p.save()
     userData = User.objects.filter(id=request.user.id)[0]
     profileData = Profile.objects.filter(id=request.user.id)[0]
+    Document = get_document_model()
+    dtiDocument = Document.objects.filter(id=profileData.dti_id)[0] if len(Document.objects.filter(id=profileData.dti_id)) else None
+    secDocument = Document.objects.filter(id=profileData.sec_id)[0] if len(Document.objects.filter(id=profileData.sec_id)) else None
+    permitDocument = Document.objects.filter(id=profileData.permit_id)[0] if len(Document.objects.filter(id=profileData.permit_id)) else None
     return render(request, 'users/profile_page.html', {
       'profileData': profileData,
       'userData': userData,
+      'dtiDocument': dtiDocument,
+      'secDocument': secDocument,
+      'permitDocument': permitDocument
     })
 
   userData = User.objects.filter(id=request.user.id)[0]
