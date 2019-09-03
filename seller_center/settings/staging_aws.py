e56186bfc7da8bec583f7af9f848de44f149ad72
lyka-jeramos <je.ramos@mylyka.com>
2019-09-03 19:06:13 +0800

    Add staging settings

--- seller_center/settings/staging_aws.py
+++ seller_center/settings/staging_aws.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = True
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
-ALLOWED_HOSTS = ['seller-center-dev.vpsbnhp5gf.ap-southeast-1.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['seller-center-staging.ap-southeast-1.elasticbeanstalk.com']
 
 
 # Database
