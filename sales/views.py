4c1a62b8e21e7ce2c9e04f287369f7442cd03458
Ramirez Joe Ramir <ramir.uplb@gmail.com>
2019-09-05 17:24:43 +0800

    SC-147 show/hide status tabs; show n orders per status tab;

--- sales/views.py
+++ sales/views.py
@@ -17,10 +17,42 @@ STATUS_CHOICES = [
 def orders(request):
 	allOrders = Order.objects.all()
 	allUsers = []
+	allUnpaidUsers = allToShipUsers = allShippingUsers = allCompletedUsers = allCancellationUsers = allReturnUsers = []
 	for order in allOrders:
 		allUsers.append(User.objects.filter(id=order.profile_id)[0].username)
+	allUnpaidOrders = Order.objects.filter(status='Unpaid')
+	for order in allOrders:
+		allUnpaidUsers.append(User.objects.filter(id=order.profile_id)[0].username)
+	allToShipOrders = Order.objects.filter(status='To Ship')
+	for order in allOrders:
+		allToShipUsers.append(User.objects.filter(id=order.profile_id)[0].username)
+	allShippingOrders = Order.objects.filter(status='Shipping')
+	for order in allOrders:
+		allShippingUsers.append(User.objects.filter(id=order.profile_id)[0].username)
+	allCompletedOrders = Order.objects.filter(status='Completed')
+	for order in allOrders:
+		allCompletedUsers.append(User.objects.filter(id=order.profile_id)[0].username)
+	allCancellationOrders = Order.objects.filter(status='Cancellation')
+	for order in allOrders:
+		allCancellationUsers.append(User.objects.filter(id=order.profile_id)[0].username)
+	allReturnOrders = Order.objects.filter(status='Return/Refund')
+	for order in allOrders:
+		allReturnUsers.append(User.objects.filter(id=order.profile_id)[0].username)
 	return render(request, 'sales/sales_page.html', {
-		'allUsersOrders': zip(allUsers, allOrders)	
+		'allUsersOrders': zip(allUsers, allOrders),
+		'allUnpaidUsersOrders': zip(allUnpaidUsers, allUnpaidOrders),
+		'allToShipUsersOrders': zip(allToShipUsers, allToShipOrders),
+		'allShippingUsersOrders': zip(allShippingUsers, allShippingOrders),
+		'allCompletedUsersOrders': zip(allCompletedUsers, allCompletedOrders),
+		'allCancellationUsersOrders': zip(allCancellationUsers, allCancellationOrders),
+		'allReturnUsersOrders': zip(allReturnUsers, allReturnOrders),
+		'nAll': len(allOrders),
+		'nUnpaid': len(allUnpaidOrders),
+		'nToShip': len(allToShipOrders),
+		'nShipping': len(allShippingOrders),
+		'nCompleted': len(allCompletedOrders),
+		'nCancellation': len(allCancellationOrders),
+		'nReturn': len(allReturnOrders),
 	})
 
 def add_order(request):
