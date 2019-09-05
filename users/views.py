54930bd7b0efa58583ebbd0608dcf8e3279c35c9
justyjusty <justyperez90@gmail.com>
2019-09-05 18:09:37 +0800

    USER: add seller account viewing in profile be

--- users/views.py
+++ users/views.py
@@ -10,18 +10,30 @@ from wagtail.documents.forms import get_document_form
 def profile(request):
   userData = User.objects.filter(id=request.user.id)[0]
   profileData = Profile.objects.filter(id=request.user.id)[0]
-  addresses = Address.objects.filter(profile_id=request.user.id)
-  Document = get_document_model()
-  dtiDocument = Document.objects.filter(id=profileData.dti_id)[0] if len(Document.objects.filter(id=profileData.dti_id)) else None
-  secDocument = Document.objects.filter(id=profileData.sec_id)[0] if len(Document.objects.filter(id=profileData.sec_id)) else None
-  permitDocument = Document.objects.filter(id=profileData.permit_id)[0] if len(Document.objects.filter(id=profileData.permit_id)) else None
+  seller_details_data=profileData.seller_details
+  shop_details_data=seller_details_data.shop_details
+  front_name=seller_details_data.upload_id_front_url
+  front_name=front_name[front_name.rfind('/')+1:]
+  seller_details_data.upload_id_front_url=front_name
+  back_name=seller_details_data.upload_id_back_url
+  back_name=back_name[back_name.rfind('/')+1:]
+  seller_details_data.upload_id_back_url=back_name
+  seller_details_data.has_agreed_to_terms='Agreed' if seller_details_data.has_agreed_to_terms else 'Not yet agreed'
+  shop_details_data.holiday_mode='On' if shop_details_data.holiday_mode else 'Off'
+  # addresses = Address.objects.filter(profile_id=request.user.id)
+  # Document = get_document_model()
+  # dtiDocument = Document.objects.filter(id=profileData.dti_id)[0] if len(Document.objects.filter(id=profileData.dti_id)) else None
+  # secDocument = Document.objects.filter(id=profileData.sec_id)[0] if len(Document.objects.filter(id=profileData.sec_id)) else None
+  # permitDocument = Document.objects.filter(id=profileData.permit_id)[0] if len(Document.objects.filter(id=profileData.permit_id)) else None
   return render(request, 'users/profile_page.html', {
     'profileData': profileData,
     'userData': userData,
-    'dtiDocument': dtiDocument,
-    'secDocument': secDocument,
-    'permitDocument': permitDocument,
-    'addresses': addresses
+    'seller_details_data': seller_details_data,
+    'shop_details_data': shop_details_data,
+    # 'dtiDocument': dtiDocument,
+    # 'secDocument': secDocument,
+    # 'permitDocument': permitDocument,
+    # 'addresses': addresses
   })
 
 def profile_edit(request):
