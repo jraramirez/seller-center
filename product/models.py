ec3f7c78482e7e6929f400a4e1cc2433c29c2cf9
JE Ramos <jeramos.dev@gmail.com>
2019-09-16 23:36:14 +0800

    Order Model: set orderReferenceNumber as primary key

--- product/models.py
+++ product/models.py
@@ -224,7 +224,7 @@ class Variations(Orderable, models.Model):
 
 @register_snippet
 class Order(models.Model):
-  orderReferenceNumber = models.CharField(null=True, blank=True, max_length=500)
+  orderReferenceNumber = models.CharField(blank=True, max_length=500, primary_key=True)
   profile = models.ForeignKey(Profile, models.DO_NOTHING, blank=True, null=True)
   total = models.CharField(null=True, blank=True, max_length=500)
   status = models.CharField(null=True, blank=True, max_length=500, default=OrderStatus.UNPAID.value)
