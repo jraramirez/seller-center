e3f1fb756f0e5ce8c576c500d8d9544ce37b42aa
jceplaras <mail@jceplaras.com>
2019-10-23 12:11:38 +0800

    Add ability to view documents of seller

--- seller_admin/views.py
+++ seller_admin/views.py
@@ -1,17 +1,36 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator
 from django.contrib.auth.models import User
+from users.models import Documents
+
 # Create your views here.
 
 USERS_PER_PAGE = 5
 
-def view_all_sellers(request):
-    context = {}
 
-    all_users = User.objects.filter(groups__name='Seller').only("username", "email").order_by('id')
+def view_all_sellers(request):
+    all_users = (
+        User.objects.filter(groups__name="Seller")
+        .only("username", "email")
+        .order_by("id")
+    )
     paginator = Paginator(all_users, USERS_PER_PAGE)
 
-    page_num = request.GET.get('page', 1)
-    context['users'] = paginator.get_page(page_num)
+    page_num = request.GET.get("page", 1)
 
-    return render(request, 'seller_admin/view_all_sellers.html', context)
+    return render(
+        request,
+        "seller_admin/view_all_sellers.html",
+        {"users": paginator.get_page(page_num)},
+    )
+
+
+def view_seller_documents(request, user_id):
+    username = User.objects.only("username").get(id=user_id)
+    docs = Documents.objects.filter(profile__user_id=user_id)
+    return render(
+        request,
+        "seller_admin/view_seller_documents.html",
+        {"documents": docs, "username": username},
+    )
+
